95585d8e2e29b1f7c12d9579574254a11c2685bf
Michael Chiem <michaelchiem1@hotmail.com>
2024-01-11 15:51:24 -0700

    Work

--- 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,17 +1,14 @@
 function keydownAction(event) {
-  // TODO: Complete keydown function
   var key = event.key.toLowerCase();
-  var allowedCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789 '.split('');
-  var arrowKeys = ['arrowup', 'arrowdown', 'arrowleft', 'arrowright'];
-  if (allowedCharacters.includes(key) || arrowKeys.includes(key)) {
-document.querySelector("#status").textContent = "KEYDOWN Event: " + key;
-}
+  var allowedKeys = 'arrowup arrowdown arrowleft arrowright'.split(' ');
+  if (allowedKeys.includes(key)) {
+    document.querySelector("#status").textContent = "KEYDOWN Event: " + key;
+  }
 }
 
 function keyupAction() {
   document.querySelector("#status").textContent = "KEYUP Event";
 }
 
-document.addEventListener("keyup", keyupAction);
-// TODO: Add Event Listener for "keydown" event
 document.addEventListener("keydown", keydownAction);
+document.addEventListener("keyup", keyupAction);
